af853ac52894650d7b6eae4e4a1e1b4d34766a10
Georgi Georgiev <vuryss@gmail.com>
2019-12-05 08:30:35 +0200

    Day 5

--- day-5.php
+++ day-5.php
@@ -9,33 +9,31 @@ echo 'Part 2: ' . compute($input, 5) . PHP_EOL;
 
 echo 'Finished in ' . (microtime(true) - $start) . PHP_EOL;
 
-function compute($program, $input) {
+function compute($prg, $input) {
     $pos = 0;
     $output = '';
     $numArgs = [1 => 3, 2 => 3, 3 => 1, 4 => 1, 5 => 2, 6 => 2, 7 => 3, 8 => 3, 99 => 0];
-    $targetArgs = [1 => 3, 2 => 3, 3 => 1, 4 => -1, 5 => -1, 6 => -1, 7 => 3, 8 => 3];
+    $posArgs = [1 => 3, 2 => 3, 3 => 1, 4 => -1, 5 => -1, 6 => -1, 7 => 3, 8 => 3];
 
     while (true) {
-        $a = str_pad((string) $program[$pos], 5, '0', STR_PAD_LEFT);
+        $a = str_pad((string) $prg[$pos], 5, '0', STR_PAD_LEFT);
         $opcode = (int) ($a[3] . $a[4]);
         $mode = [1 => (int) $a[2], 2 => (int) $a[1], 3 => (int) $a[0]];
         $arg = [];
 
         for ($i = 1; $i <= $numArgs[$opcode]; $i++) {
-            $arg[$i] = $mode[$i] || $targetArgs[$opcode] === $i
-                        ? (int) $program[$pos + $i]
-                        : (int) $program[$program[$pos + $i]];
+            $arg[$i] = $mode[$i] || $posArgs[$opcode] === $i ? (int) $prg[$pos + $i] : (int) $prg[$prg[$pos + $i]];
         }
 
         switch ($opcode) {
             case 1:
-                $program[$arg[3]] = (int) $arg[1] + (int) $arg[2];
+                $prg[$arg[3]] = (int) $arg[1] + (int) $arg[2];
                 break;
             case 2:
-                $program[$arg[3]] = (int) $arg[1] * (int) $arg[2];
+                $prg[$arg[3]] = (int) $arg[1] * (int) $arg[2];
                 break;
             case 3:
-                $program[$arg[1]] = $input;
+                $prg[$arg[1]] = $input;
                 break;
             case 4:
                 $output .= $arg[1];
@@ -52,10 +50,10 @@ function compute($program, $input) {
                 }
                 break;
             case 7:
-                $program[$arg[3]] = $arg[1] < $arg[2] ? 1 : 0;
+                $prg[$arg[3]] = $arg[1] < $arg[2] ? 1 : 0;
                 break;
             case 8:
-                $program[$arg[3]] = $arg[1] === $arg[2] ? 1 : 0;
+                $prg[$arg[3]] = $arg[1] === $arg[2] ? 1 : 0;
                 break;
 
             case 99:
