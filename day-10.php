d5dd2787788c436013f0c89e03c4e2945e62542a
Georgi Georgiev <vuryss@gmail.com>
2019-12-11 06:40:08 +0200

    Day 10

--- day-10.php
+++ day-10.php
@@ -39,71 +39,40 @@ echo 'Part 1: ' . $part1 . PHP_EOL;
 echo 'Part 2: ' . $part2 . PHP_EOL;
 
 function killOthers($map, $x, $y) {
-    $lines = [];
+    $angles = [];
 
     foreach ($map as $x1 => $yLine) {
         foreach ($yLine as $y1 => $item) {
-            if ($x1 === $x && $y1 === $y) {
-                continue;
-            }
+            if ($x1 === $x && $y1 === $y) continue;
 
-            $lines[] = [($x1 - $x), ($y1 - $y)];
+            $line = [($x1 - $x), ($y1 - $y)];
+            $angle = atan2($line[1], $line[0]) + M_PI_2;
+            $dist  = abs($line[0]) + abs($line[1]);
+
+            if ($angle < 0) $angle += 2*M_PI;
+
+            $angles[(string) $angle][$dist] = [$x1, $y1];
         }
     }
 
-    $map = [];
+    ksort($angles);
 
-    foreach ($lines as $key => $line) {
-        if ($line[0] >= 0) {
-            if ($line[1] < 0) {
-                $angle = atan(bcdiv($line[0], abs($line[1]), 3));
-                // < 90
-            } elseif ($line[1] === 0) {
-                $angle = M_PI_2;
-            } else {
-                if ($line[0] === 0) {
-                    // = 180
-                    $angle = M_PI;
-                } else {
-                    // > 90 < 180
-                    $angle = M_PI_2 + atan(bcdiv($line[1], $line[0], 3));
-                }
-            }
-        } else {
-            if ($line[1] > 0) {
-                $angle = M_PI + atan(bcdiv(abs($line[0]), $line[1], 3));
-                // > 180 < 270
-            } elseif ($line[1] === 0) {
-                $angle = M_PI + M_PI_2;
-            } else {
-                $angle = M_PI + M_PI_2 + atan(bcdiv(abs($line[1]), abs($line[0]), 3));
-                // > 270 < 360
-            }
-        }
-
-        $dist = abs($line[0]) + abs($line[1]);
-
-        $map[(string) $angle][$dist] = [$line[0] + $x, $line[1] + $y];
-    }
-
-    ksort($map);
-
-    foreach ($map as $rad => $items) {
+    foreach ($angles as $rad => $items) {
         ksort($items);
-        $map[$rad] = $items;
+        $angles[$rad] = $items;
     }
 
     $counter = 0;
 
     while (true) {
-        foreach ($map as $rad => $items) {
+        foreach ($angles as $rad => $items) {
             $counter++;
             $item = array_shift($items);
             if ($counter === 200) {
                 return 100 * $item[0] + $item[1];
             }
             if (empty($items)) {
-                unset($map[$rad]);
+                unset($angles[$rad]);
             }
         }
     }
@@ -111,7 +80,7 @@ function killOthers($map, $x, $y) {
 
 function detectOthers($map, $x, $y)
 {
-    $lines = [];
+    $angles = [];
 
     foreach ($map as $x1 => $yLine) {
         foreach ($yLine as $y1 => $item) {
@@ -119,58 +88,11 @@ function detectOthers($map, $x, $y)
                 continue;
             }
 
-            $lines[] = [($x1 - $x), ($y1 - $y)];
+            $angles[(string) atan2($y - $y1, $x1 - $x)] = true;
         }
     }
 
-    $checked = [];
-
-    $change = true;
-    while ($change) {
-        $change = false;
-
-        foreach ($lines as $key => $line) {
-            if (isset($checked[$key])) continue;
-            $checked[$key] = true;
-
-            foreach ($lines as $key2 => $line2) {
-                if ($key === $key2) continue;
-
-                if (($line[0] < 0 && $line2[0] > 0)
-                    || ($line[0] > 0 && $line2[0] < 0)
-                    || ($line[1] < 0 && $line2[1] > 0)
-                    || ($line[1] > 0 && $line2[1] < 0)
-                ) {
-                    continue;
-                }
-
-                if ($line[0] === 0 && $line2[0] === 0 || $line[1] === 0 && $line2[1] === 0) {
-                    unset($lines[$key2]);
-                    $change = true;
-                    continue;
-                }
-
-                if ($line2[0] === 0 || $line2[1] === 0 || $line[0] === 0 || $line[1] === 0) {
-                    continue;
-                }
-
-                $a1 = bcdiv($line[0], $line[1], 3);
-                $a2 = bcdiv($line2[0], $line2[1], 3);
-
-                if ($a1 === $a2) {
-                    unset($lines[$key2]);
-                    $change = true;
-                    continue;
-                }
-            }
-
-            if ($change) {
-                break;
-            }
-        }
-    }
-
-    return count($lines);
+    return count($angles);
 }
 
 
