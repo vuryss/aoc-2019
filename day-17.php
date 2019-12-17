d403675175035a07ec10dc2fd361f8f7ca3e5e09
Georgi Georgiev <vuryss@gmail.com>
2019-12-17 11:23:13 +0200

    Day 17

--- day-17.php
+++ day-17.php
@@ -9,176 +9,123 @@ $start = microtime(true);
 $comp = new IntCodeParser($input);
 
 $map = [];
-$y = $x = 0;
+$y = $x = $rX = $rY = 0;
 $maxX = $maxY = 0;
 
 while (true) {
-    $output = $comp->output();
+    if (!$output = $comp->output()) break;
 
-    if ($output === null) {
-        break;
-    }
-
-    if ($output === 35) {
-        $map[$y][$x++] = chr($output);
-    } elseif ($output === 46) {
-        $map[$y][$x++] = chr($output);
-    } elseif ($output === 10) {
-        $y++;
+    if ($output === 10) {
         $x = 0;
-    } else {
-        $map[$y][$x++] = chr($output);
+        $y++;
+        continue;
     }
+
+    if ($output === 94) {
+        $rX = $x;
+        $rY = $y;
+    }
+
+    $map[$y][$x++] = chr($output);
 
     if ($x > $maxX) $maxX = $x;
-    if ($y > $maxY) $maxY = $y;
 }
 
+$maxY = $y;
 $sum = 0;
-$rX = $rY = 0;
-$dir = 'U';
 
 for ($y = 0; $y <= $maxY; $y++) {
     for ($x = 0; $x <= $maxX; $x++) {
-
-        if (($map[$y][$x] ?? '') === '#') {
-            if (isset($map[$y+1][$x]) && $map[$y+1][$x] === '#'
-                && isset($map[$y-1][$x]) && $map[$y-1][$x] === '#'
-                && isset($map[$y][$x+1]) && $map[$y][$x+1] === '#'
-                && isset($map[$y][$x-1]) && $map[$y][$x-1] === '#'
-            ) {
-                $sum += $x * $y;
-            }
-        }
-
-        if (($map[$y][$x] ?? '') === '^') {
-            $rX = $x;
-            $rY = $y;
+        if (($map[$y][$x] ?? '') === '#'
+            && ($map[$y+1][$x] ?? '') === '#' && ($map[$y-1][$x] ?? '') === '#'
+            && ($map[$y][$x+1] ?? '') === '#' && ($map[$y][$x-1] ?? '') === '#'
+        ) {
+            $sum += $x * $y;
         }
     }
 }
 
 echo 'Part 1: ' . $sum . PHP_EOL;
 
-$avail = [
-    'U' => ['L', 'U', 'R'],
-    'L' => ['D', 'L', 'U'],
-    'R' => ['U', 'R', 'D'],
-    'D' => ['R', 'D', 'L'],
-];
-$deltaX = [
-    'U' => 0,
-    'L' => -1,
-    'R' => 1,
-    'D' => 0,
-];
-$deltaY = [
-    'U' => -1,
-    'L' => 0,
-    'R' => 0,
-    'D' => 1,
-];
+$dir = 'U';
+
+$turns = ['U' => ['L', 'R'], 'L' => ['D', 'U'], 'R' => ['U', 'D'], 'D' => ['R', 'L']];
+$deltaX = ['U' => 0, 'L' => -1, 'R' => 1, 'D' => 0];
+$deltaY = ['U' => -1, 'L' => 0, 'R' => 0, 'D' => 1];
 
 $steps = [];
-
 $moves = 0;
-[$x, $y] = [$rX, $rY];
+$lastTurn = '';
 
 while (true) {
-    // Check forward
-    $next = $map[$y + $deltaY[$dir]][$x + $deltaX[$dir]] ?? '';
+    $next = $map[$rY + $deltaY[$dir]][$rX + $deltaX[$dir]] ?? '';
     if ($next === '#') {
         $moves++;
-        $y += $deltaY[$dir];
-        $x += $deltaX[$dir];
+        $rY += $deltaY[$dir];
+        $rX += $deltaX[$dir];
         continue;
     }
 
-    // Check Left
-    $left = $map[$y + $deltaY[$avail[$dir][0]]][$x + $deltaX[$avail[$dir][0]]] ?? '';
-    if ($left === '#') {
-        $steps[] = $moves;
-        $steps[] = 'L';
-        $dir   = $avail[$dir][0];
-        $moves = 1;
-        $y     += $deltaY[$dir];
-        $x     += $deltaX[$dir];
-        continue;
+    foreach ($turns[$dir] as $turn => $newDir) {
+        $next = $map[$rY + $deltaY[$newDir]][$rX + $deltaX[$newDir]] ?? '';
+        if ($next === '#') {
+            if ($moves) $steps[] = $lastTurn . $moves;
+            $lastTurn = $turn ? 'R' : 'L';
+            $dir   = $newDir;
+            $moves = 1;
+            $rY    += $deltaY[$dir];
+            $rX    += $deltaX[$dir];
+            continue 2;
+        }
     }
 
-    // Check Right
-    $right = $map[$y + $deltaY[$avail[$dir][2]]][$x + $deltaX[$avail[$dir][2]]] ?? '';
-    if ($right === '#') {
-        $steps[] = $moves;
-        $steps[] = 'R';
-        $dir   = $avail[$dir][2];
-        $moves = 1;
-        $y     += $deltaY[$dir];
-        $x     += $deltaX[$dir];
-        continue;
-    }
-
-    $steps[] = $moves;
+    $steps[] = $lastTurn . $moves;
     break;
-}
-
-array_shift($steps);
-
-function norm($steps) {
-    $tuples = [];
-
-    for ($i = 0, $to = count($steps); $i < $to; $i += 2) {
-        $tuples[] = $steps[$i] . $steps[$i+1];
-    }
-
-    return $tuples;
 }
 
 function combineIntoThree($steps, $level = 1, $found = [])
 {
     $string = implode(',', $steps);
+    $stepsCount = count($steps);
 
     $subSteps = [];
 
-    //echo '---------- LEVEL --------- ' . $level . ' ---------------- ' . PHP_EOL;
-
     foreach ($steps as $step) {
+        $subStepsCount = count($subSteps) + 1;
         if ($step === '0') {
-            if (count($subSteps) === 1) break;
+            if ($subStepsCount === 2) break;
             $subSteps = [];
             continue;
         }
         $subSteps[] = $step;
 
-        if (count($subSteps) < 2) continue;
+        if ($subStepsCount < 2) continue;
 
         $substr2 = implode(',', $subSteps);
-        $count = substr_count($string, $substr2);
-        if ($count === 1) break;
+        if (substr_count($string, $substr2) === 1) break;
 
-        $copy = $steps;
-        for ($i = 0; $i <= count($steps) - count($subSteps); $i++) {
+        $stepsCopy = $steps;
+        for ($i = 0, $to = $stepsCount - $subStepsCount; $i <= $to; $i++) {
             foreach ($subSteps as $j => $subStep) {
                 if ($steps[$i + $j] !== $subStep) continue 2;
             }
 
             foreach ($subSteps as $j => $subStep) {
-                $copy[$i + $j] = '0';
+                $stepsCopy[$i + $j] = '0';
             }
+
+            $i += $subStepsCount -1;
         }
-
-
-        //echo 'Substring: ' . $substr2 . ' counts: ' . $count . PHP_EOL;
-        //echo 'Replaced string: ' . implode(',', $copy) . PHP_EOL;
 
         if ($level < 3) {
             $foundCopy = $found;
             $foundCopy[] = $substr2;
-            $result = combineIntoThree($copy, $level + 1, $foundCopy);
 
-            if ($result) return $result;
+            if ($result = combineIntoThree($stepsCopy, $level + 1, $foundCopy)) {
+                return $result;
+            }
         } elseif ($level === 3) {
-            if (strspn(implode(',', $copy), '0,') === strlen(implode(',', $copy))) {
+            if (!count(array_diff($stepsCopy, ['0']))) {
                 $found[] = $substr2;
                 return $found;
             }
@@ -188,9 +135,9 @@ function combineIntoThree($steps, $level = 1, $found = [])
     return null;
 }
 
-$subCombinations = combineIntoThree(norm($steps));
+$subCombinations = combineIntoThree($steps);
 
-$routine = implode(',', norm($steps));
+$routine = implode(',', $steps);
 foreach ($subCombinations as $index => $subCombination) {
     $routine = str_replace($subCombination, chr(ord('A') + $index), $routine);
 }
@@ -206,9 +153,7 @@ $comp->input(10);
 foreach ($subCombinations as $subCombination) {
     foreach (str_split($subCombination) as $char) {
         $comp->input(ord($char));
-        if ($char === 'L' || $char === 'R') {
-            $comp->input(ord(','));
-        }
+        if ($char === 'L' || $char === 'R') $comp->input(ord(','));
     }
     $comp->input(10);
 }
